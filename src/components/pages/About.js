9ae1a0d8760dd6e74d2db7839e0de20fe7ead0f0
Chelsea Burnham <chelseaburnham0@gmail.com>
2022-08-25 15:48:06 -0700

    added about function to return about me

---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function About() {
+    return (
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="..." alt="Professional Photo" />
+                <div class="card-body">
+                    <h5 class="card-title">About Me</h5>
+                    <p class="card-text">This is my about me.</p>
+                </div>
+        </div>
+    );
+};
+
+export { About }
